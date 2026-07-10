cae87a83976ebe95063df01edd1c37cf65b4936f
PetersonSantos9K <petersonsantos9k@gmail.com>
2026-07-09 23:29:22 -0300

    feat: referenciando os posts do usuario

--- src/main/java/io/github/PetersonSantos9K/workshopmongo/domain/User.java
+++ src/main/java/io/github/PetersonSantos9K/workshopmongo/domain/User.java
@@ -3,24 +3,37 @@ package io.github.PetersonSantos9K.workshopmongo.domain;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
-@Getter
-@Setter
+
 @Document(collection = "user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Getter
+    @Setter
     @Id
     private String id;
+    @Getter
+    @Setter
     private String name;
+    @Getter
+    @Setter
     private String email;
+
+    @Getter
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
 
     public User(){}
 
@@ -29,6 +42,7 @@ public class User implements Serializable {
         this.name = name;
         this.email = email;
     }
+
 
 
     @Override
